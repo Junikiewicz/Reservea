533bbadb7846f20e7e971ed1952814e92738ad06
Junikiewicz <32637113+Junikiewicz@users.noreply.github.com>
2020-11-03 00:03:57 +0100

    Test CI/CD

--- Reservea.API/Reservea.Microservices/Reservea.Microservices.Resources/Controllers/WeatherForecastController.cs
+++ Reservea.API/Reservea.Microservices/Reservea.Microservices.Resources/Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace Reservea.Microservices.Resources.Controllers
             })
             .ToArray();
         }
+        
+        [HttpGet("test"]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
